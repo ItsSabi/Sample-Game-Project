c599467d8b9ec22ed1b4799fbcad2fbce21d703f
S R <sabi.sabi@gmail.com>
2019-07-19 14:06:26 -0500

    Add files via upload
    Scripts used to make objects move, quit the game, and make it run.

---
+++ EndTrigger.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.CompleteLevel();
+    }
+
+
+
+
+
+
+}
---
+++ PlayerCollision.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+       
+            //Disable player movement when they crash
+            movement.enabled = false;
+            //Ends the game
+            FindObjectOfType<GameManager>().EndGame();
+
+
+
+
+        }
+
+    }
+
+
+
+}
